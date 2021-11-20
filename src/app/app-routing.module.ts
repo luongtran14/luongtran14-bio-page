7ee701ff293fad7672c22ec53f030900c3d4a0ec
luongtran14 <luongtranduc2001@gmail.com>
2021-11-20 10:44:57 +0700

    update functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./main-page/main-page.component";
+import {SidePageComponent} from "./side-page/side-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main-page', component: MainPageComponent
+  },
+  {
+    path: 'side-page', component: SidePageComponent
+  },
+  {
+    path: '', redirectTo:'main-page', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
